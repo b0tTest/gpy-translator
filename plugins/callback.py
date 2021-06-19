691818ff710a630907acbfc665ffc79229171d37
samadii <77415087+samadii@users.noreply.github.com>
2021-06-19 17:00:56 +0430

    Create callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -16,11 +16,6 @@ async def helpbutton(bot: Client, query: CallbackQuery):
     await query.message.edit(constants.help_text, reply_markup=constants.help_markup)
 
 
-@Client.on_callback_query(filters.regex(r"^Credits$"))
-async def credits(bot: Client, query: CallbackQuery):
-    await query.answer(constants.credits, show_alert=True)
-
-
 @Client.on_callback_query(filters.regex(r"^closethismsg$"))
 async def close_error_message_callback(bot: Client, query: CallbackQuery):
     await query.message.delete()
